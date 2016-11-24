e2ab7b2d530f1c31fb10921f3fc1a407674330dc
Imran Ismail <imran.codely@gmail.com>
2016-11-24 13:53:06 +0800

    Prepare for release

--- mix.exs
+++ mix.exs
@@ -7,25 +7,29 @@ defmodule Turbolinks.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp description do
+    """
+    A plug and play package for using Turbolinks with Elixir web frameworks
+    """
+  end
+
+  defp package do
+    [name: :turbolinks,
+     files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+     maintainers: ["Imran Ismail"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/imranismail/turbolinks"}]
+  end
+
   defp deps do
     [{:phoenix, "~> 1.2.1", optional: true},
      {:plug, "~> 1.2", optional: true}]
