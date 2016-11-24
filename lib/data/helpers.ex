4e562958bb7946676bf4a6cc5bc7ee5f6da1f839
Imran Ismail <imran.codely@gmail.com>
2016-11-24 13:47:43 +0800

    Add turbolinks helper functions

---
+++ lib/data/helpers.ex
@@ -0,0 +1,40 @@
+defmodule Turbolinks.Helpers do
+  @moduledoc """
+  Helper functions for using with frameworks
+  """
+
+  @doc """
+  Custom redirection method to return a turbolinks visit and clearcache
+  """
+  def redirect(conn, opts) do
+    if xhr?(conn) do
+      js(conn, turbolinks_resp(opts[:to], conn.method))
+    else
+      Phoenix.Controller.redirect(conn, opts)
+    end
+  end
+
+  @doc """
+  Custom js response, great to be used to build hybrid turbolinks app
+  """
+  def js(conn, data) do
+    conn
+    |> Plug.Conn.put_resp_content_type(Plug.MIME.type("js"))
+    |> Plug.Conn.send_resp(conn.status || 200, data)
+  end
+
+  defp xhr?(conn) do
+    conn
+    |> Plug.Conn.get_req_header("x-requested-with")
+    |> List.first
+    |> Kernel.==("XMLHttpRequest")
+  end
+
+  defp turbolinks_resp(to, "GET"), do: "Turbolinks.visit('#{to}');"
+  defp turbolinks_resp(to, _) do
+    """
+    Turbolinks.clearCache();
+    Turbolinks.visit('#{to}');
+    """
+  end
+end
